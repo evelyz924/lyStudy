d890b3e827a9450595f0a1fde914e269d7df83f2
evelyz924 <evelyz924@gmail.com>
2018-07-16 14:09:36 +0900

    0716 14.09

---
+++ work/asset/b_step_02_jquery/js/src/0716tabMenu.js
@@ -0,0 +1,22 @@
+// 0716tabMenu.js
+
+(function($){
+
+	// tab_box 내의 li클릭시
+	var tabBox = $('.tab_box');
+	var tabLi  = tabBox.find('li');
+	var tabText = $('.tab_text').children('div');
+
+	tabLi.on('click',function(e) {
+		e.preventDefault();
+		var i = $(this).index();
+
+		// 선택한 li에 .select적용
+		tabLi.eq(i).addClass('select');
+		tabLi.eq(i).siblings().removeClass('select');
+
+  	// tab_text내의 순서에 맞는 내용에 .select적용
+  	tabText.eq(i).addClass('select');
+  	tabText.eq(i).siblings().removeClass('select');
+	});
+})(jQuery);
